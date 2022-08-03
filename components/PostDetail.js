cc3ba2f397e26853adb8dc014cc183c958d96a07
pdduypham <pdduypham@gmail.com>
2022-08-03 15:04:59 +0700

    #22: Detail information of the product.

--- components/PostDetail.js
+++ components/PostDetail.js
@@ -1,12 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect, useLayoutEffect } from 'react'
+import { ScrollView, StyleSheet, Text, View, Image, Dimensions } from 'react-native'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import colors from '../constants/colors'
 import fonts from '../constants/fonts'
 import { Avatar } from 'react-native-elements'
-import  firebase  from '@react-native-firebase/app'
+import firebase from '@react-native-firebase/app'
 import storage from '@react-native-firebase/storage'
 
 const PostDetail = ({ navigation, route }) => {
+
+    const [listImages, setListImages] = useState([])
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -19,12 +21,23 @@ const PostDetail = ({ navigation, route }) => {
         })
     })
 
-    useEffect(()=> {
-        console.log(await firebase.storage().ref(postImages).listAll()
-            .items.pop().getDownloadURL().then((url) => {
-                url
-        }))
-    })
+    useEffect(() => {
+        const fetchImages = async () => {
+            if (route.params.postImages != 'No image') {
+                const imageRef = await firebase.storage().ref(route.params.postImages).listAll()
+                const urls = await Promise.all(imageRef.items.map(ref => ref.getDownloadURL()))
+                return urls
+            } else {
+                console.log('blo')
+            }
+        }
+
+        const loadImages = async () => {
+            const urls = await fetchImages();
+            setListImages(urls);
+        }
+        loadImages();
+    }, [route])
 
     return (
         <View style={styles.container}>
@@ -38,12 +51,33 @@ const PostDetail = ({ navigation, route }) => {
                 color: 'red'
             }}>{route.params.postPrice} đ</Text>
 
+            {route.params.postImages != 'No image'
+                && <ScrollView horizontal>
+                    {listImages.map((item, index) =>
+                        <View key={index} style={{
+                            width: Dimensions.get('window').width,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            paddingLeft: 20,
+                            paddingRight: 35
+                        }}>
+                            <Image source={{ uri: item }}
+                                style={{
+                                    width: '100%',
+                                    height: 300,
+                                }}
+                                resizeMethod='resize'
+                                resizeMode='contain'
+                            />
+                        </View>
+                    )}
+                </ScrollView>}
             <View>
                 <Avatar rounded size={50}
                     source={require('../assets/logo.jpg')} />
-                
+
             </View>
-            <Text >{route.params.postImages}</Text>
+            <Text >{route.params.postDisplayName}</Text>
         </View>
 
     )
@@ -53,7 +87,6 @@ export default PostDetail
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         backgroundColor: 'white',
         paddingTop: 20,
         paddingLeft: 10
